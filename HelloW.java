b48229972208dc1f7592b63a01803f2b55875f66
Angraaz <63395289+Angraaz@users.noreply.github.com>
2020-04-28 12:29:14 +0530

    Update HelloW.java

--- HelloW.java
+++ HelloW.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
        public static void main(String[] argv)
        {
-                System.out.println("Hello world..");
+                System.out.println("Hello world to coding club..");
        }
 }
